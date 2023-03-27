a1d2d6e604f60414b5f632f0b8de28ba8dee1bec
Alex Noir <Varsash@gmail.com>
2023-03-28 01:29:59 +0300

    Add showname test

--- dialog_view.gd
+++ dialog_view.gd
@@ -2,10 +2,12 @@ extends Control
 
 
 func _on_set_button_pressed():
+	$DialogBox.set_showname($LineEdit.text)
 	$DialogBox.set_msg($MsgEdit.text)
 	$DialogBox.letter_delay = $DelaySpinBox.value
 
 
 func _on_add_button_pressed():
+	$DialogBox.set_showname($LineEdit.text)
 	$DialogBox.add_msg($MsgEdit.text)
 	$DialogBox.letter_delay = $DelaySpinBox.value
