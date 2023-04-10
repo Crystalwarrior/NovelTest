0976bbca36762df4c39e520cfb532f3407fa2d9b
Alex Noir <Varsash@gmail.com>
2023-04-11 02:40:42 +0300

    add fade in/fade out test, make zoombutton script more versatile

--- ZoomButton.gd
+++ ZoomButton.gd
@@ -2,10 +2,11 @@ extends Button
 
 
 @export var zoom_animation: AnimationPlayer
+@export var animation_to_play: String = "zoom"
 
 
 func _on_toggled(button_pressed):
 	if button_pressed:
-		zoom_animation.play("zoom")
+		zoom_animation.play(animation_to_play)
 	else:
-		zoom_animation.play_backwards("zoom")
+		zoom_animation.play_backwards(animation_to_play)
